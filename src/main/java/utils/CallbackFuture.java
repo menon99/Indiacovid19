f874449244b0b236fdd454e8b3372e261918dc42
Akash <akashmenon11699cool@hotmail.com>
2020-04-23 15:22:14 +0530

    utilty class for callback using future

---
+++ src/main/java/utils/CallbackFuture.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
+
+public class CallbackFuture extends CompletableFuture<Response> implements Callback {
+    public void onResponse(Call call, Response response) {
+         super.complete(response);
+    }
+    public void onFailure(Call call, IOException e){
+         super.completeExceptionally(e);
+    }
+}
