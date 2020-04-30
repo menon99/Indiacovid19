76f3be874a808e1e7c8a1014689f314d5ea595a7
Akash <akashmenon11699cool@hotmail.com>
2020-04-30 16:22:03 +0530

    added functionality to return JSONArrays

--- src/main/java/utils/GetJSON.java
+++ src/main/java/utils/GetJSON.java
@@ -2,6 +2,8 @@ package utils;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+
+import org.json.JSONArray;
 import org.json.JSONObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -22,11 +24,29 @@ public class GetJSON {
 		JSONObject j = new JSONObject(responseBody);
 		return j;
 	}
+	
+	private JSONArray getJSONArray(Response res) throws IOException {
+		String responseBody = res.body().string();
+		JSONArray j = new JSONArray(responseBody);
+		return j;
+	}
 
 	public JSONObject getJSONAsync(CallbackFuture future) throws InterruptedException, ExecutionException {
 		JSONObject j1 = (JSONObject) future.thenApply(res -> {
 			try {
 				return this.getJSON(res);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return 0;
+		}).get();
+		return j1;
+	}
+	
+	public JSONArray getJSONArrayAsync(CallbackFuture future) throws InterruptedException, ExecutionException {
+		JSONArray j1 = (JSONArray) future.thenApply(res -> {
+			try {
+				return this.getJSONArray(res);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
