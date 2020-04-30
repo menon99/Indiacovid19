474c9d4043721149a41409e22c84ef2650d99a7f
Akash <akashmenon11699cool@hotmail.com>
2020-04-30 12:37:13 +0530

    added testing data

--- src/main/java/Home.java
+++ src/main/java/Home.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.concurrent.ExecutionException;
 
 import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 import utils.CallbackFuture;
@@ -22,14 +23,59 @@ public class Home extends HttpServlet {
 			throws ServletException, IOException {
 		
 		GetJSON gj = new GetJSON();
-		JSONObject j1 = null;
+		JSONObject j1 = null,j2 = null;
 		CallbackFuture f1;
+		
 		try {
 			f1 = gj.getResponse("https://api.covid19india.org/data.json");
 			j1 = gj.getJSONAsync(f1);
 		} catch (InterruptedException | ExecutionException | IOException e) {
 			e.printStackTrace();
 		}
+		
+		try {
+			f1 = gj.getResponse("https://api.covid19india.org/state_test_data.json");
+			j2 = gj.getJSONAsync(f1);
+		}catch (InterruptedException | ExecutionException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		JSONArray tabledata = j1.getJSONArray("statewise");
+		JSONArray testingData = j2.getJSONArray("states_tested_data");
+		
+//		System.out.println("table data is ");
+//		System.out.println(tabledata);
+//		System.out.println("Testing data is ");
+//		System.out.println(testingData);
+		
+		int k = 0;
+		while(k != testingData.length()) {
+			
+			String state = testingData.getJSONObject(k).getString("state");
+			int totalTested = Integer.parseInt(testingData.getJSONObject(k).getString("totaltested"));
+			
+			while(k != testingData.length() - 1 && state.equalsIgnoreCase(testingData.getJSONObject(k + 1).getString("state"))) {
+				try {
+				totalTested += Integer.parseInt(testingData.getJSONObject(k + 1).getString("totaltested"));
+				}catch (NumberFormatException e){
+					//
+				}
+				k++;
+			}
+			
+			String testedOn = testingData.getJSONObject(k).getString("updatedon");
+			
+			for(int i = 1; i < tabledata.length(); i ++) {
+				if(state.equalsIgnoreCase(tabledata.getJSONObject(i).getString("state"))) {
+					tabledata.getJSONObject(i).put("tested", totalTested);
+					tabledata.getJSONObject(i).put("updatedOn", testedOn);
+					break;
+				}
+			}
+			
+			k++;
+		}
+		
 		
 		ServletContext ctx = request.getServletContext();
 		
@@ -41,7 +87,7 @@ public class Home extends HttpServlet {
 		JSONObject indiaStates = new JSONObject(content);
 		
 		request.setAttribute("india", indiaStates);
-		request.setAttribute("tableData", j1.getJSONArray("statewise"));
+		request.setAttribute("tableData", tabledata);
 		
 		RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
 		rd.forward(request, response);
