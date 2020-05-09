1a23d48a95fe0325037c995b3bc82cb1a52949c3
Akash <akashmenon11699cool@hotmail.com>
2020-05-10 01:48:28 +0530

    fixed bugs

--- src/main/java/utils/StringFormatter.java
+++ src/main/java/utils/StringFormatter.java
@@ -4,7 +4,11 @@ public class StringFormatter {
 	public static String capitalizeWord(String str){  
 	    String words[]=str.split("\\s");  
 	    String capitalizeWord="";  
-	    for(String w:words){  
+	    for(String w:words){
+	    	if(w.equalsIgnoreCase("and")) {
+	    		capitalizeWord += "and ";
+	    		continue;
+	    	}
 	        String first=w.substring(0,1);  
 	        String afterfirst=w.substring(1);  
 	        capitalizeWord+=first.toUpperCase()+afterfirst+" ";  
