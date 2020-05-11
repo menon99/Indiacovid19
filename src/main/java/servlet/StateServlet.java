b2721ff16a34b247dd0874c58a3ece6e3ba2bb8d
Akash <akashmenon11699cool@hotmail.com>
2020-05-12 01:48:36 +0530

    added containment zones

--- src/main/java/servlet/StateServlet.java
+++ src/main/java/servlet/StateServlet.java
@@ -21,10 +21,11 @@ import utils.GetTrends;
 import utils.StringFormatter;
 
 public class StateServlet extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 //		String [] parts = request.getRequestURI().split("/");
 //		String state = null;
 //		if(parts.length == 4) {
@@ -32,79 +33,96 @@ public class StateServlet extends HttpServlet {
 //			System.out.println("state is " + state);
 //		}
 		String state = request.getParameter("state");
+
 		System.out.println("state is " + state);
+		System.out.println("states before format is " + state);
+		state = StringFormatter.capitalizeWord(state);
+		System.out.println("state after format is " + state);
+
 		GetJSON gj = new GetJSON();
-		CallbackFuture f1;
-		JSONArray j1 = null;
+		CallbackFuture f1, f2;
+		JSONArray j1 = null, j2 = null;
 		try {
-			 f1 = gj.getResponse("https://api.covid19india.org/v2/state_district_wise.json");
-			 j1 = gj.getJSONArrayAsync(f1);
+			f1 = gj.getResponse("https://api.covid19india.org/v2/state_district_wise.json");
+			f2 = gj.getResponse("https://api.covid19india.org/zones.json");
+			j1 = gj.getJSONArrayAsync(f1);
+			j2 = gj.getJSONAsync(f2).getJSONArray("zones");
 		} catch (InterruptedException | ExecutionException | IOException e) {
 			e.printStackTrace();
 		}
 		
-		JSONArray districts = new JSONArray();
+		int start_index = 0, end_index;
+		while (!state.equalsIgnoreCase(j2.getJSONObject(start_index).getString("state")))
+			start_index++;
+		end_index = start_index + 1;
+		while (state.equalsIgnoreCase(j2.getJSONObject(end_index).getString("state")))
+			end_index++;
+		end_index -=1;
 		
-		for(int i = 0; i < j1.length(); i++) {
-			if(state.equalsIgnoreCase(j1.getJSONObject(i).getString("state"))) {
-				
+		JSONArray districts = new JSONArray();
+
+		for (int i = 0; i < j1.length(); i++) {
+			if (state.equalsIgnoreCase(j1.getJSONObject(i).getString("state"))) {
+
 				JSONArray data = j1.getJSONObject(i).getJSONArray("districtData");
-				int c = 0,a = 0,r = 0,d = 0;
-				
-				for(int k = 0; k < data.length(); k++) {
-					
+				int c = 0, a = 0, r = 0, d = 0;
+
+				for (int k = 0; k < data.length(); k++) {
+
 					JSONObject obj = data.getJSONObject(k);
-					if(!obj.getString("district").equalsIgnoreCase("Unknown")) {
+					if (!obj.getString("district").equalsIgnoreCase("Unknown")) {
+						for(int kk = start_index; kk <= end_index;kk++) {
+							if(obj.getString("district").equalsIgnoreCase(j2.getJSONObject(kk).getString("district"))) {
+								obj.put("zone", j2.getJSONObject(kk).get("zone"));
+								break;
+							}
+						}
 						districts.put(obj);
 					}
-					
+
 					c += obj.getInt("confirmed");
 					r += obj.getInt("recovered");
 					d += obj.getInt("deceased");
 					a += obj.getInt("active");
 				}
-				
+
 				JSONObject total = new JSONObject();
 				total.put("district", "total");
 				total.put("active", a);
 				total.put("recovered", r);
 				total.put("confirmed", c);
 				total.put("deceased", d);
-				
+
 				districts.put(total);
 				break;
 			}
 		}
 		request.setAttribute("tabledata", districts);
-		
+
 		GetTrends gt = new GetTrends();
 		JSONObject trends = gt.getTrends();
 		JSONArray dates = gt.getDates();
-		
-		System.out.println("states before format is " + state);
-		state = StringFormatter.capitalizeWord(state);
-		System.out.println("state after format is " + state);
-		
+
 		request.setAttribute("dates", dates);
 		request.setAttribute("trends", trends.getJSONObject(state));
-		
-		String [] parts = state.toLowerCase().split(" ");
+
+		String[] parts = state.toLowerCase().split(" ");
 		String jsonName = String.join("", parts) + ".json";
-		
-		System.out.println("json name is "+ jsonName);
-		
+
+		System.out.println("json name is " + jsonName);
+
 		ServletContext ctx = request.getServletContext();
-		
-		String path = ctx.getRealPath("/resources/json/" + jsonName); //Deployment
-		
+
+		String path = ctx.getRealPath("/resources/json/" + jsonName); // Deployment
+
 		File file = new File(path);
 		String content = FileUtils.readFileToString(file, "utf-8");
-		
+
 		JSONObject stateCoord = new JSONObject(content);
-		
+
 		request.setAttribute("stateCoords", stateCoord);
 		request.setAttribute("sname", state);
-		
+
 		RequestDispatcher rd = request.getRequestDispatcher("states.jsp");
 		rd.forward(request, response);
 	}
